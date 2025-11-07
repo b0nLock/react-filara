03c3d9cd6655d9aa94d94a8ae7e191b3bed53b9e
b0nLock <b1n13eaker@gmail.com>
2025-11-07 16:08:14 +0300

    Icon package and props

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,27 @@ import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { useState } from "react";
 
 function App() {
+  const [isAuthorized, setIsAuthorized] = useState(false)
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        {isAuthorized && <Route path="/" element={<Home />} />}
+        <Route
+          path="/login"
+          element={<Login setIsAuthorized={setIsAuthorized} />}
+        />
+        <Route
+          path="/register"
+          element={<Register setIsAuthorized={setIsAuthorized} />}
+        />
+        {isAuthorized ? (
+          <Route path="*" element={<Navigate to="/" />} />
+        ) : (
+          <Route path="*" element={<Navigate to="/login" />} />
+        )}
       </Routes>
     </>
   );
